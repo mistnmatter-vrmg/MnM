a64a10fb76b6f0410a3c32a32a4e87685a9e4d7e
mistnmatter-vrmg <mistnmatter@gmail.com>
2026-02-25 11:02:13 +0530

    Fix product names - Add Fabric Perfume suffix to all products

---
+++ fix-all-product-names.sql
@@ -0,0 +1,5 @@
+-- Fix all product names to include "Fabric Perfume"
+UPDATE stock SET product_name = 'Royal Cotton Fabric Perfume' WHERE product_name = 'Royal Cotton';
+UPDATE stock SET product_name = 'White Tea & Woods Fabric Perfume' WHERE product_name = 'White Tea & Woods';
+UPDATE stock SET product_name = 'Soft Cotton Cloud Fabric Perfume' WHERE product_name = 'scc';
+UPDATE stock SET product_name = 'Ivory Linen Fabric Perfume' WHERE product_name = 'Ivory Linen';
